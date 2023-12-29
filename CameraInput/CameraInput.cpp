3585402188cc201469a2ae6354bb31d73f4ee2f1
LucasBergholz <lucas.bergholz@gmail.com>
2023-12-29 17:09:35 -0300

    Adicionando multiplos raios ao codigo da camera

--- CameraInput/CameraInput.cpp
+++ CameraInput/CameraInput.cpp
@@ -67,41 +67,66 @@ int main() {
             if (contourSize >= maxContour) { maxContour = contourSize; biggestContour = count2; }
             count2++;
         }
-        drawContours(output, contours, biggestContour, Scalar(0, 255, 0), 3);
+        drawContours(output, contours, -1, Scalar(0, 255, 0), 3);
 
-        int x = 0;
-        int y = 0;
-        count2 = 0;
+        //Achar os centroides
+        struct centroides { // Struct que guardara os centroids
+            vector<Point2f> centroids; //Vetor que guarda todos os centroids encontrados
+            vector<int> visited; //Vetor que guarda se o centroid foi visitado ou nao
+        };
 
-        for (auto i : contours) {
-            for (auto j : contours[biggestContour]) {
-                if (contours[biggestContour].size()) {
-                    x = x + contours[biggestContour][count2].x;
-                    y = y + contours[biggestContour][count2].y;
+        struct centroides c; // Inicializacao da struct
+        c.centroids.resize(contours.size()); // Inicializacao do vetor centroids com o tamanho do numero de contours q existem
+        c.visited.resize(contours.size(), 0); // Inicializando com valor 0 todos os membros do vetor visited
+
+        for (size_t i = 0; i < contours.size(); ++i) {
+            // Calcular os moments
+            Moments mu = moments(contours[i]);
+            // Calcular coordenadas dos centroides
+            c.centroids[i].x = mu.m10 / mu.m00;
+            c.centroids[i].y = mu.m01 / mu.m00;
+            // Draw a circle at the centroid
+            //circle(output, c.centroids[printed], 3, Scalar(0, 0, 255), -1);
+            //circle(greyFrame, c.centroids[printed], 3, Scalar(0, 0, 255), -1);
+
+            // Print das coordenadas
+            cout << "Centroid of Contour " << i << ": (" << c.centroids[i].x << ", " << c.centroids[i].y << ")" << endl;
+        }
+
+        //Discriminar centroides
+        vector<Point2f> finalCentroids(contours.size()); //Vetor que guarda os centroids apos discriminacao
+        for (size_t i = 0; i < c.centroids.size(); i++) {
+            int biggestPoint = i;
+            if (!c.visited[i]) { // Se o centroid nao tiver sido visitado ainda, compare ele com os demais
+                for (size_t j = i + 1; j < c.centroids.size(); j++) {
+                    if (c.visited[j]) continue; // Se o centroid ja foi visitado, pula pro proximo
+                    cout << abs(c.centroids[i].x - c.centroids[j].x) << "  " << abs(c.centroids[i].y - c.centroids[j].y) << endl;
+                    if (abs(c.centroids[i].x - c.centroids[j].x) <= 30 && abs(c.centroids[i].y - c.centroids[j].y) <= 30) { // abs serve para achar o modulo, se o modulo da diferenca for menor/igual a 20 entra
+                        if (contours[biggestPoint].size() < contours[j].size()) biggestPoint = j;
+                        c.visited[j] = 1;
+                    }
                 }
-                count2++;
+                finalCentroids[i] = c.centroids[biggestPoint];
             }
-            break;
+            c.visited[i] = 1;
         }
 
-        if (count2 > 0) { printf("%d e %d\n", x / count2, y / count2); }
-        if (x != 0 && y != 0) {
-            Point p(x / count2, y / count2);
-            circle(output, p, 3, Scalar(0, 0, 255), -1);
+        for (size_t i = 0; i < finalCentroids.size(); ++i) {
+            if ((finalCentroids[i].x + finalCentroids[i].y == 0)) continue;
+            circle(output, finalCentroids[i], 3, Scalar(0, 0, 255), -1);
+            circle(greyFrame, finalCentroids[i], 3, Scalar(0, 0, 255), -1);
         }
 
-        // coordinates of centroid
         printf("%d:\n\n\n\n", countOfFrames);
 
         imshow("frame cinza", greyFrame);
         imshow("oie", output);
 
-        string filename = "C:\\Users\\Lucas Bergholz\\Documents\\GAMA CUBE DESIGN\\CameraInput\\camera_video\\frame_" + to_string(countOfFrames-1) + ".png";
-        remove(filename.c_str());
         waitKey(0);
+        string filename = "C:\\Users\\Lucas Bergholz\\Documents\\GAMA CUBE DESIGN\\CameraInput\\camera_video\\frame_" + to_string(countOfFrames - 1) + ".png";
+        remove(filename.c_str());
 
     }
-
     waitKey(0);
 
     return 0;
